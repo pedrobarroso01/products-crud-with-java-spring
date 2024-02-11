e8035ae8f29b48ac017a915575edcfe840797219
pedrobarroso01 <pedrobarrosocontato@gmail.com>
2024-02-11 19:10:59 -0300

    Conectando ao Banco de Dados e criando Migrations

--- CRUD/src/main/java/com/livre/crud/demo/controllers/ProductController.java
+++ CRUD/src/main/java/com/livre/crud/demo/controllers/ProductController.java
@@ -1,4 +1,15 @@
 package com.livre.crud.demo.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Products")
 public class ProductController {
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity getAllProducts () {
+        return ResponseEntity.ok("deu certo");
+    }
 }
